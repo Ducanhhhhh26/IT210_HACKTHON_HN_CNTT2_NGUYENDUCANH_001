c87c0c96bd820cb3e13773e21236b21593f76bfd
DucAnh <ducanhcualo2403@gmail.com>
2026-04-20 09:52:09 +0700

    update code

--- src/main/java/org/com/hackthon/config/WebInit.java
+++ src/main/java/org/com/hackthon/config/WebInit.java
@@ -2,27 +2,21 @@ package org.com.hackthon.config;
 
 import org.jspecify.annotations.Nullable;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-import jakarta.servlet.MultipartConfigElement;
-import jakarta.servlet.ServletRegistration;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
 public class WebInit extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?> @Nullable [] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[] { WebConfig.class };
     }
 
     @Override
     protected Class<?> @Nullable [] getServletConfigClasses() {
-        return new Class[] { WebConfig.class , org.com.hackthon.config.I18NConfiguration.class};
+        return new Class[0] ;
     }
 
     @Override
     protected String[] getServletMappings() {
         return new String[] { "/" };
-    }
-
-    @Override
-    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
-        registration.setMultipartConfig(new MultipartConfigElement("", 5242880, 20971520, 0));
     }
 }
